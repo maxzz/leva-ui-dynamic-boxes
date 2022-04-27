acf15dbb329b42b4a234f10f23fcb247882e2d51
Max Zakharzhevskiy <maxzz@msn.com>
2022-04-27 15:41:42 -0700

    WIP. add boxes project

---
+++ README.md
@@ -0,0 +1,6 @@
+#### Refs
+
+* [https://github.com/pmndrs/leva/pull/226](https://github.com/pmndrs/leva/pull/226)
+* [https://ci.codesandbox.io/status/pmndrs/leva](https://ci.codesandbox.io/status/pmndrs/leva)
+* [https://codesandbox.io/s/leva-ui-fhhcf8?file=/src/App.jsx](https://codesandbox.io/s/leva-ui-fhhcf8?file=/src/App.jsx)
+* [https://codesandbox.io/s/leva-ui-dynamic-boxes-mf2k5w?file=/src/Box.jsx](https://codesandbox.io/s/leva-ui-dynamic-boxes-mf2k5w?file=/src/Box.jsx)
---
+++ src/Box.tsx
@@ -0,0 +1,105 @@
+import React, { useEffect, useCallback } from 'react';
+import { folder, useControls, useCreateStore } from 'leva';
+import { useDropzone } from 'react-dropzone';
+import { useDrag } from '@use-gesture/react';
+
+export function Box({ index, selected, setSelect }) {
+    const store = useCreateStore();
+
+    const [{ position, size, color, fillColor, fillMode, fillImage, width }, set] = useControls(
+        () => ({
+            position: {
+                value: [window.innerWidth / 2 - 150, window.innerHeight / 2],
+                step: 1,
+            },
+            size: { value: { width: 100, height: 100 }, min: 10, lock: true },
+            fillMode: { value: 'color', options: ['image'] },
+            fillColor: {
+                value: '#cfcfcf',
+                label: 'fill',
+                render: (get) => get('fillMode') === 'color',
+            },
+            fillImage: {
+                image: undefined,
+                label: 'fill',
+                render: (get) => get('fillMode') === 'image',
+            },
+            stroke: folder({ 
+                color: '#555555', 
+                width: { value: 1, min: 0, max: 10 } }
+            ),
+        }),
+        { store }
+    );
+
+    const bind = useDrag(({ first, movement: [x, y], args: controls, memo = { position, size } }) => {
+        if (first) setSelect([index, store]);
+        let _position = [...memo.position];
+        let _size = { ...memo.size };
+
+        controls.forEach(([control, mod]) => {
+            switch (control) {
+                case 'position':
+                    _position[0] += x;
+                    _position[1] += y;
+                    break;
+                case 'width':
+                    _size.width += x * mod;
+                    if (mod === -1) _position[0] += x;
+                    break;
+                case 'height':
+                    _size.height += y * mod;
+                    if (mod === -1) _position[1] += y;
+                    break;
+                default:
+            }
+        });
+        set({ position: _position, size: _size });
+        return memo;
+    });
+
+    useEffect(() => {
+        setSelect([index, store]);
+    }, [index, store, setSelect]);
+
+    const onDrop = useCallback(
+        (acceptedFiles) => {
+            if (!acceptedFiles.length) return;
+            set({ fillImage: acceptedFiles[0], fillMode: 'image' });
+        },
+        [set]
+    );
+
+    const { getRootProps, isDragAccept } = useDropzone({
+        maxFiles: 1,
+        accept: 'image/*',
+        onDrop,
+    });
+
+    const background = fillMode === 'color' || !fillImage ? fillColor : `center / cover no-repeat url(${fillImage})`;
+
+    return (
+        <div
+            {...getRootProps()}
+            tabIndex={index}
+            className={`box ${selected ? 'selected' : ''}`}
+            style={{
+                background,
+                width: size.width,
+                height: size.height,
+                boxShadow: `inset 0 0 0 ${width}px ${color}`,
+                transform: `translate(${position[0]}px, ${position[1]}px)`,
+            }}
+        >
+            <span className="handle top" {...bind(['height', -1])} />
+            <span className="handle right" {...bind(['width', 1])} />
+            <span className="handle bottom" {...bind(['height', 1])} />
+            <span className="handle left" {...bind(['width', -1])} />
+            <span className="handle corner top-left" {...bind(['width', -1], ['height', -1])} />
+            <span className="handle corner top-right" {...bind(['width', 1], ['height', -1])} />
+            <span className="handle corner bottom-left" {...bind(['width', -1], ['height', 1])} />
+            <span className="handle corner bottom-right" {...bind(['width', 1], ['height', 1])} />
+            <span className="handle position" {...bind(['position'])} style={{ background: isDragAccept ? '#18a0fb66' : 'transparent' }} />
+        </div>
+    );
+}
