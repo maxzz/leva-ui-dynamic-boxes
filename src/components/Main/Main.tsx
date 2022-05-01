d73fe8298ad9a8dd7b9afef42898736d695f43a9
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-01 15:28:21 -0700

    WIP. box. update app styles layout

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Leva, useControls, LevaPanel, button } from 'leva';
+import { StoreType } from 'leva/dist/declarations/src/types';
+import { Box, BoxData } from '../Box';
+import './Main.scss';
+
+export function Main() {
+    const [boxes, setBoxes] = React.useState<BoxData[]>([]);
+    const [[selection, store], setSelection] = React.useState<[index: number, store: StoreType | null]>([-1, null]);
+
+    React.useEffect(() => {
+        function deleteSelection(e: KeyboardEvent) {
+            if (e.key === 'Backspace' && selection > -1 && (e.target as HTMLElement)?.classList.contains('selected')) {
+                setBoxes((b) => {
+                    const _b = [...b];
+                    _b.splice(selection, 1);
+                    return _b;
+                });
+                setSelection([-1, null]);
+            }
+        }
+        window.addEventListener('keydown', deleteSelection);
+
+        return () => window.removeEventListener('keydown', deleteSelection);
+    }, [selection]);
+
+    const unSelect = (e: React.MouseEvent) => {
+        if (e.target === e.currentTarget) {
+            setSelection([-1, null]);
+        }
+    };
+
+    const addBox = () => {
+        setBoxes((boxes) => [...boxes, {
+            index: Date.now(),
+            // store: useCreateStore(),
+            store: null,
+        }]);
+    };
+
+    useControls({ 'New Box': button(addBox) });
+
+    return (
+        <div className="wrapper">
+            <div className="canvas" onClick={unSelect}>
+                {boxes.map((box, idx) => (
+                    // <Box index={idx} store={box.store} selected={selection === idx} setSelect={setSelection} key={box.index} />
+                    <Box index={idx} selected={selection === idx} setSelect={setSelection} key={box.index} />
+                ))}
+            </div>
+            <div className="panel">
+                <Leva fill flat titleBar={false} />
+                <LevaPanel store={store} fill flat titleBar={false} />
+            </div>
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 /*18*/
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
 /**/
 /*17* /
 import React from 'react';
