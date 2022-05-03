9e4135f62278f1015860fa602705b9ab77a627e9
Max Zakharzhevskiy <maxzz@msn.com>
2022-05-03 15:28:43 -0700

    WIP. box. add Box2WithStore wrapper

--- src/components/Box/BodyHandlesWithSore.tsx
+++ src/components/Box/BodyHandlesWithSore.tsx
@@ -1,13 +1,29 @@
-import { useCreateStore } from "leva";
+import { folder, useControls, useCreateStore } from "leva";
+import { StoreType } from "leva/dist/declarations/src/types";
 import { useState } from "react";
 import { BodyHandles, Position } from "./BodyHandles";
-import { BoxBody, BoxProps } from "./BoxBody";
+import { BoxProps } from "./BoxBody";
 
-export function Box2WithStore(props: Omit<BoxProps, 'store'>) {
-    const store = useCreateStore();
-    // return <BoxBody {...props} store={store} />;
+export function Box2Controls({ store }: { store: StoreType; }) {
 
-    const [position, setPosition] = useState<Position>([0,0,100,100]);
+    const [{ position, size, strokeColor, fillColor, fillMode, fillImage, width }, set] = useControls(() => (
+        {
+            position: { value: [window.innerWidth / 2 - 150, window.innerHeight / 2], step: 1, },
+            size: { value: { width: 100, height: 100 }, min: 10, lock: true },
+
+            style: folder({
+                fillMode: { value: 'color', options: ['image'] },
+                fillColor: { value: '#ff8600', label: 'fill-color', render: (get) => get('style.fillMode') === 'color', },
+                fillImage: { image: undefined, label: 'fill image', render: (get) => get('style.fillMode') === 'image', },
+
+                strokeColor: { value: '#555555', label: 'stroke-color' },
+                width: { value: 1, min: 0, max: 10, step: 1, label: 'stroke-width' },
+            }),
+        }),
+        { store }
+    );
+
+    const [position2, setPosition2] = useState<Position>([0, 0, 100, 100]);
     // function setPosition(v: Position) {
     //     console.log('setPosition', v);
     // }
@@ -15,7 +31,17 @@ export function Box2WithStore(props: Omit<BoxProps, 'store'>) {
     // function setSelected(v: boolean) {
     //     console.log('setSelected', v);
     // }
+
     return (
-        <BodyHandles position={position} setPosition={setPosition} selected={selected} setSelected={setSelected} />
+        <BodyHandles position={position2} setPosition={setPosition2} selected={selected} setSelected={setSelected} />
+    );
+}
+
+export function Box2WithStore(props: Omit<BoxProps, 'store'>) {
+    const store = useCreateStore();
+    // return <BoxBody {...props} store={store} />;
+
+    return (
+        <Box2Controls {...props} store={store} />
     );
 };
